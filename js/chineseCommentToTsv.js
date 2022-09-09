496966aafe57b76868c5b4d360393844a66b2fbd
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-09-09 11:45:25 +0800

    Create chineseCommentToTsv.js

---
+++ js/chineseCommentToTsv.js
@@ -0,0 +1,12 @@
+((rows = '') => {
+    let tsv = rows.split('\n').map(row => {
+        row = row.split(' ');
+        row.shift();
+        row = row.join('');
+        row = row.split(':').join('    ');
+        row = row.split('：').join('    ');
+        return(row);
+    }).join('\n');
+    copy(tsv);
+    return(tsv);
+})(`1. 民「瘼」：音ㄇㄛˋ`/* 國文注釋之多行字串 */);
