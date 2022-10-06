9bb32425663a16cc1dd495ada62c5c9a7961ea83
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-10-07 00:05:34 +0800

    Update printColab.js

--- js/printColab.js
+++ js/printColab.js
@@ -10,46 +10,51 @@ printColab.js
     [ ] 頁首及頁尾
     [v] 背景圖形
 */
-let style = document.createElement('style');
-let scroller = document.querySelectorAll('colab-shaded-scroller')[0];
-style.innerHTML = `
-colab-shaded-scroller {
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    height: auto;
-    width: 100vw;
-    overflow-x: hidden;
-    overflow-y: auto;
-}
-.notebook-scrolling-horizontal {
-    padding-bottom: 0px;
-}
-colab-status-bar {
-    opacity: 0;
-}
-colab-shaded-scroller .notebook-content {
-    margin: 0px;
-}
-colab-shaded-scroller :where(.footer-links, .add-cell) {
-    display: none !important;
-}
-.output_subarea.output_text > pre { /* in iframe */
-    width: 100% !important;
-    white-space: unset !important;
-    overflow-wrap: normal !important;
-}
-@page{
-    margin: auto;
-    size: A4 portrait;
-    marks: none;
-    bleed: 0in;
-}
-`;
-scroller.remove();
-document.body.innerHTML = '';
-document.body.appendChild(scroller);
-document.body.appendChild(style);
-for(let element of document.querySelectorAll('style[media="screen"]')){
-    element.setAttribute('media', 'all');
-}
+(function(){
+    let style = document.createElement('style');
+    let scroller = document.querySelectorAll('colab-shaded-scroller')[0];
+    style.innerHTML = `
+    colab-shaded-scroller {
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        height: auto;
+        width: 100vw;
+        overflow-x: hidden;
+        overflow-y: auto;
+    }
+    .notebook-scrolling-horizontal {
+        padding-bottom: 0px;
+    }
+    colab-status-bar {
+        opacity: 0;
+    }
+    colab-shaded-scroller .notebook-content {
+        margin: 0px;
+    }
+    colab-shaded-scroller :where(.footer-links, .add-cell) {
+        display: none !important;
+    }
+    .output_subarea.output_text > pre { /* in iframe */
+        width: 100% !important;
+        white-space: unset !important;
+        overflow-wrap: normal !important;
+    }
+    @page{
+        margin: auto;
+        size: A4 portrait;
+        marks: none;
+        bleed: 0in;
+    }
+    `;
+    scroller.remove();
+    document.body.innerHTML = '';
+    document.body.appendChild(scroller);
+    document.body.appendChild(style);
+    for(let element of document.querySelectorAll('style[media="screen"]')){
+        element.setAttribute('media', 'all');
+    }
+    setTimeout(() => {
+        print();
+    }, 1e2);
+})();
