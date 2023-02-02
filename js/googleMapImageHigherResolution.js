69528eb6591e2d3581ccd5383778f1a98a713e14
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2023-02-02 21:10:22 +0800

    提高 GoogleMap 圖像解析度

--- js/googleMapImageHigherResolution.js
+++ js/googleMapImageHigherResolution.js
@@ -3,6 +3,7 @@
  * 提高 GoogleMap 圖像解析度
  * googleMapImageHigherResolution.js
  */
+
 href = location.href.split('=');
 href[1] = href[1].replaceAll(/(w|h|s)([0-9]*)-/g, (...args) => {
     return(args[1] + (args[2]*2).toString() + '-');
