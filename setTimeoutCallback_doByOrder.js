4b5b9161d58b8c7ccdc1746952d1341b1bc5c50f
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2023-02-04 00:35:33 +0800

    希望能改成印出 a, b, c

---
+++ setTimeoutCallback_doByOrder.js
@@ -0,0 +1,84 @@
+// question
+(() => {
+    /*
+    ** a, b, c 是三個 request 的 callback function，
+    ** 分別會延遲 500, 200 300 毫秒回來
+    ** 執行 doByOrder 之後會後依序印出 b, c, a
+    ** 希望能改成印出 a, b, c
+    */ 
+    
+    // Don't change a, b, c function
+    const a = callback => {
+        setTimeout(() => { callback('a'); }, 500);
+    };
+    const b = callback => {
+        setTimeout(() => { callback('b'); }, 200);
+    };
+    const c = callback => {
+        setTimeout(() => { callback('c'); }, 300);
+    };
+    const tasks = [a, b, c];
+    const doByOrder = (tasks, callback) => {
+        tasks.forEach(item => {
+            item(callback);
+        });
+    }
+    doByOrder(tasks, console.log.bind(console));
+})();
+
+// MaoHuPi's answer
+(() => {
+    const a = callback => {
+        setTimeout(() => { callback('a'); }, 500);
+    };
+    const b = callback => {
+        setTimeout(() => { callback('b'); }, 200);
+    };
+    const c = callback => {
+        setTimeout(() => { callback('c'); }, 300);
+    };
+    const tasks = [a, b, c];
+    const doByOrder = (tasks, callback) => {
+        /* 2023 © MaoHuPi */
+        function linkFunctions(...functions){
+            return((...args) => {
+                for(let func of functions){
+                    func(...args);
+                }
+            });
+        }
+        let funcAll = callback;
+        for(let item of tasks.reverse()){
+            let lastAll = funcAll;
+            funcAll = linkFunctions(callback, function(){item(lastAll);});
+        }
+        funcAll();
+    }
+    doByOrder(tasks, console.log.bind(console));
+})();
+
+// regular answer
+(() => {
+    const a = callback => {
+        setTimeout(() => { callback('a'); }, 500);
+    };
+    const b = callback => {
+        setTimeout(() => { callback('b'); }, 200);
+    };
+    const c = callback => {
+        setTimeout(() => { callback('c'); }, 300);
+    };
+    const tasks = [a, b, c];
+        const doByOrder = (tasks, callback) => {
+            let i = 0;
+            const next = () => {
+                if (i >= tasks.length) return;
+                    tasks[i++]((val) => {
+                    callback(val);
+                    next();
+                });
+            };
+            next();
+        };
+    doByOrder(tasks, console.log.bind(console));
+})();
