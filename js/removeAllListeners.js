b0d538cf5ccad50185cbe74f6671639d02afcbcf
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2023-05-14 22:57:35 +0800

    移除所有事件監聽器

---
+++ js/removeAllListeners.js
@@ -0,0 +1,19 @@
+/* 
+ * 2023 © MaoHuPi
+ * removeAllListeners.js
+ * 移除所有事件監聽器
+ */
+(() => {
+    function removeAllListeners(element){
+        let eventListeners = getEventListeners(element);
+        for(let type in eventListeners){
+            eventListeners[type].forEach(event => {
+                // event.remove();
+                element.removeEventListener(type, event.listener, event.useCapture);
+            });
+        }
+    }
+    removeAllListeners(window);
+    removeAllListeners(document);
+    [...document.querySelectorAll('*')].forEach(element => removeAllListeners(element));
+})();
